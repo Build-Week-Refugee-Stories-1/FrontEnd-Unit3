0eda69dfe490e31baa08818d2148d8d26ce176c5
Chris Daley <Daleys@Chriss-iMac.local>
2020-01-06 22:12:01 -0700

    Created Cards and Grids for stories. Still need to work out some kinks

--- stories/src/components/Stories.js
+++ stories/src/components/Stories.js
@@ -1,21 +1,52 @@
-import React from 'react';
-import { PubContext } from '../contexts/PubStories';
+import React from "react";
+import { PubContext } from "../contexts/PubStories";
+import { Grid, Card, Container, Image, Icon, } from "semantic-ui-react";
 
 const Stories = props => {
-    const story = React.useContext(PubContext)
+  const story = React.useContext(PubContext);
 
-    return (
-        <>
-            {story.map(stuff => (
-                <div>
-                                    <p>{stuff.name}</p>
-                <p>{stuff.story}</p>
-                <p>{stuff.quote}</p>
-                </div >
-            ))}
-        </> 
-    )
-}
-    
+  return (
+    <>
+      <Container>
+        <Card fluid>
+          <Card.Content>
+            <Card.Header>Refugee Stories</Card.Header>
+          </Card.Content>
+          <Card.Content>
+            <Grid columns={4}>
+              <Grid.Row>
+                {story.map(stuff => (
+                  <Grid.Column key={stuff.id}>
+                    <Card color="black">
+                      <Card.Content>
+                        <Card.Header>{stuff.name}</Card.Header>
+                      </Card.Content>
+                      <br />
+                      <Card.Content>
+                        <Card.Description>{stuff.story}</Card.Description>
+                      </Card.Content>
+                      <br />
+                      <Card.Content>
+                        <Card.Header>{stuff.quote}</Card.Header>
+                      </Card.Content>
+                    </Card>
+                  </Grid.Column>
+                ))}
+              </Grid.Row>
+            </Grid>
+          </Card.Content>
+        </Card>
+      </Container>
+    </>
+  );
+};
 
 export default Stories;
+
+// {story.map(stuff => (
+//   <div>
+//     <p>{stuff.name}</p>
+//     <p>{stuff.story}</p>
+//     <p>{stuff.quote}</p>
+//   </div>
+// ))}
